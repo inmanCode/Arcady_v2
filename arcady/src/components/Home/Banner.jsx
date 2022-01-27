09c4ca46b3e83e39f054bb9dadd08fe8c94c2b95
Aymane Abssi <aymane.abssi@dotenv.it>
2022-01-27 18:48:51 +0100

    finished banner animation

--- arcady/src/components/Home/Banner.jsx
+++ arcady/src/components/Home/Banner.jsx
@@ -5,6 +5,12 @@ import {
   WhiteText,
   GreenText,
   Icon,
+  MetaMaskContainer,
+  RevealMask,
+  RevealText,
+  RevealTextInline,
+  Mask,
+  Btn,
 } from './styles/index';
 import Button from '../../../styles/globalComponents/Button';
 import Link from 'next/link';
@@ -34,23 +40,40 @@ const Banner = ({ blok }) => {
           .filter((item) => item.component === 'HBW_RT')
           .map((item, i) =>
             item.HBW_RT?.content.map((text, i) => (
-              <WhiteText key={i}>{render(text.content[0].text)}</WhiteText>
+              <MetaMaskContainer key={i}>
+                <RevealMask i={i === 0 ? 0.5 : i == 1 ? 0.8 : 1.1}>
+                  <RevealText>
+                    {render(text.content[0].text)}
+                    <RevealTextInline i={i === 0 ? 1.3 : i == 1 ? 1.8 : 2.1}>
+                      {render(text.content[0].text)}
+                    </RevealTextInline>
+                  </RevealText>
+                </RevealMask>
+              </MetaMaskContainer>
             ))
           )}
+
         {blok?.grid
           .filter((item) => item.component === 'HBG_RT')
           .map((item, i) =>
             item.HBG_RT?.content.map((text, i) => (
-              <GreenText key={i}>{render(text.content[0].text)}</GreenText>
+              <MetaMaskContainer key={i}>
+                <GreenText key={i} i={i}>
+                  {render(text.content[0].text)}
+                  <Mask i={i} />
+                </GreenText>
+              </MetaMaskContainer>
             ))
           )}
         {blok?.grid
           .filter((item) => item.component === 'HBB')
           .map((item, i) => {
             return (
-              <Link href='#readmore' key={i} passHref>
-                <Button>{item.HBB.content[0].content[0].text}</Button>
-              </Link>
+              <MetaMaskContainer key={i}>
+                <Link href='#readmore' key={i} passHref>
+                  <Btn>{item.HBB.content[0].content[0].text}</Btn>
+                </Link>
+              </MetaMaskContainer>
             );
           })}
         <Icon href='#readmore'>
--- arcady/src/components/Home/styles/index.js
+++ arcady/src/components/Home/styles/index.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components'
+import styled, { keyframes, css } from 'styled-components'
 
 const chevAnimation = keyframes`
   0%,20%,50%,80%,100% {
@@ -11,7 +11,61 @@ const chevAnimation = keyframes`
     transform: translateY(3px);
   }
 `;
-
+const matrixA = keyframes`
+    from{
+      transform: matrix(1, 0, 0, 1, 0, 100);
+    }
+    to {
+      transform: matrix(1, 0, 0, 1, 0, 0);
+    }
+`
+const BtnA = keyframes`
+    0%{
+      transform: matrix(1, 0, 0, 1, 0, 100);
+    }
+    50% {
+      transform: matrix(1, 0, 0, 1, 0, -10);
+    }
+    100% {
+      transform: matrix(1, 0, 0, 1, 0, 0);
+    }
+`
+const clipPathA = keyframes`
+    from{
+      clip-path: polygon(0% 100%, 0% 100%, 0% 0%, 0% 0%);
+    }
+    to {
+      clip-path: polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%);
+      }
+`
+const clipPathBgA = keyframes`
+    from{
+      clip-path: polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%);
+    }
+    to {
+      clip-path: polygon(100% 100%, 100% 100%, 100% 0%, 100% 0%);
+      }
+`
+const VisbilityA = keyframes`
+    from{
+      visibility: hidden;
+        }
+    to {
+      visibility: visible
+            }
+`
+const Matrixanimation = props =>
+     css`
+       ${matrixA} .3s forwards ${props.i}s ;
+`
+const animation = props =>
+     css`
+       ${clipPathA} .5s forwards ${props.i}s ;
+`
+const OverLayanimation = props =>
+     css`
+       ${clipPathBgA} .3s forwards 2.6s ;
+`
 export const Container = styled.div`
     display: flex;
     min-height: 100vh;
@@ -39,7 +93,7 @@ export const InnerContainer = styled.div`
     flex-direction: column;
     flex-wrap: wrap;
     justify-content: center;
-    `;
+`;
 
 export const WhiteText = styled.div`
     color: white;
@@ -54,15 +108,80 @@ export const GreenText = styled.div`
     font-size: 2em;
     font-weight: 500;
     line-height: 1.2;
+    position: relative;
+    width:fit-content;
+    visibility: hidden;
+    animation: ${VisbilityA} .3s forwards 2.1s;
 `
 export const Icon = styled.a`
     position: absolute;
-    height:100%;
     left: 50%;
-    top: 45%;
+    top: 90%;
     display: flex;
     align-items: center;
     justify-content: center;
     animation: ${chevAnimation} infinite 2s;
     cursor: pointer;
 `;
+
+
+export const MetaMaskContainer = styled.div`
+    overflow: hidden;
+    `
+export const RevealMask = styled.div`
+    transform: matrix(1, 0, 0, 1, 0, 1000);
+    animation: ${Matrixanimation};
+  `
+export const RevealText = styled.div`
+    position: relative;
+    color: #8f8f8f;
+    font-size: 3em;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 800;
+    line-height: 1.2;
+`
+export const RevealTextInline = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: white;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 800;
+    line-height: 1.2;
+    clip-path: polygon(0% 100%, 0% 100%, 0% 0%, 0% 0%);
+    animation: ${animation};
+`
+export const Mask = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: #fff;
+  width:100%;
+  height:100%;
+  clip-path: polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%);
+  animation: ${OverLayanimation};
+`
+export const Btn = styled.div`
+    height: 40px;
+    line-height: 40px;
+    font-family: "Montserrat";
+    font-size: 1.5rem;
+    font-weight: 600;
+    padding: 0 40px;
+    color: #fff;
+    text-transform: uppercase;
+    background-color: #00a667;
+    border-radius: 20px;
+    border: 0;
+    outline: 0;
+    cursor: pointer;
+    text-decoration: none;
+    transition: background-color .2s ease;
+    display: inline-block;
+    white-space: nowrap;
+    -webkit-appearance: none;
+    align-self: flex-start;
+    margin-top: 40px;
+    transform: matrix(1, 0, 0, 1, 0, 100);
+    animation: ${BtnA} 1s forwards 2.8s;
+`
